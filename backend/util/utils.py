9dc64cccc57941b3b5e4e3a4109f93fa607703ea
felixu <xufei_0320@163.com>
2020-11-05 14:59:30 +0800

    :ok_hand: 统一在获取请求数据时放入当前 owner

--- backend/util/utils.py
+++ backend/util/utils.py
@@ -1,4 +1,5 @@
 import json
+from backend.util.jwt_token import UserHolder
 from testing_platform.settings import LOGGER
 from backend.exception.error_code import ErrorCode
 from backend.exception.exception import PlatformError
@@ -9,6 +10,7 @@ def full_data(request, method):
     从请求中获取请求体
     """
     # 判断请求方法是否符合要求
+    data = {}
     if request.method != method:
         raise PlatformError.error_args(ErrorCode.HTTP_METHOD_NOT_SUPPORTED, method)
     # POST 和 PUT json 从 body 中取 form data 从 POST 中取
@@ -20,6 +22,9 @@ def full_data(request, method):
     # DELETE 和 GET 从 GET 中取
     if method == 'GET' or method == 'DELETE':
         return request.GET
+    # 由于从 GET 或者 POST 中取出的字典 QueryDict 为不可变对象，需要执行 copy(源码注释中提供的信息) 方法
+    data = data.copy()
+    data.update({'owner': UserHolder.current_user()})
 
 
 def get_params(data, *args, toleration=False):
@@ -61,8 +66,8 @@ def page_params(data, *args):
     # 取字典中的分页参数
     page, page_size, *values = get_params(data, 'page', 'page_size', *args, toleration=True)
     # 不存在则填充默认值
-    if page is None or page <= 0:
+    if page is None or int(page) <= 0:
         page = 1
-    if page_size is None or page_size <= 0:
+    if page_size is None or int(page_size) <= 0:
         page_size = 10
     return page, page_size, *values
