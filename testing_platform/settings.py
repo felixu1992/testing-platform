659807b5f8079245c15fbb378e2abafe5433edcc
felixu <xufei_0320@163.com>
2020-11-08 08:57:13 +0800

    :wrench: 去除配置

--- testing_platform/settings.py
+++ testing_platform/settings.py
@@ -83,17 +83,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
         'NAME': 'testing_platform',  # 数据库名称
-        'USER': 'root',  # 连接数据库的用户名称
-        'PASSWORD': 'felixu19920320++-',  # 用户密码
-        'HOST': '47.92.233.129',  # 访问的数据库的主机的ip地址
-        'PORT': '3306',  # 默认mysql访问端口
+        'USER': '',  # 连接数据库的用户名称
+        'PASSWORD': '',  # 用户密码
+        'HOST': '',  # 访问的数据库的主机的ip地址
+        'PORT': '',  # 默认mysql访问端口
     }
 }
 
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://47.92.233.129:6379',
+        'LOCATION': '',
         'OPTIONS': {
             'CONNECTION_POOL_KWARGS': {'max_connections': 100},
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
